90b3e14b2a4c2ebd6f5446479902b8b8a24ebc94
simphotonics <git@simphotonics.com>
2021-11-18 23:08:41 +0000

    Added import statements.

--- src/unitexpr/__init__.py
+++ src/unitexpr/__init__.py
@@ -3,12 +3,18 @@ Units, unit expressions and united numpy arrays.
 
 The package `unitexpr` provides the modules:
 
-- unit: Module containing base classes `UnitBase` and `UnitMeta`.
-- unit_symbol: Class used to specify base unit symbols.
-- unit_array: A united version of a numpy array.
+- unit: Module containing the base classes `UnitBase` and `UnitMeta`.
+- unit_symbol: Provides the class `UnitSymbol` used to specify
+  base unit symbols.
+- qarray: A sub-class of a `numpy` n-dimensional array whose entries
+  represent a quantity with an associated unit.
 - si_units: Predefined units representing the SI Unit System.
 - sc_units: Predefined units based on (nm, ps, m_e, K, cd, ). Semiconductor
             Units.
 
 
 """
+
+from .unit import UnitBase, UnitMeta
+from .unit_symbol import UnitSymbol
+from .qarray import QArray
