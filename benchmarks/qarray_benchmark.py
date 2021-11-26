b78e2ac649e5696a1a1bf2d7ec85f09cde10b650
simphotonics <git@simphotonics.com>
2021-11-26 11:23:02 +0000

    Changed benchmark params.

--- benchmarks/qarray_benchmark.py
+++ benchmarks/qarray_benchmark.py
@@ -15,14 +15,12 @@ ny = 200
 A = ndarray(shape=(nx, ny))
 A.fill(10.0)
 
-M = QArray(shape=(nx, ny), unit=m ** 2)
-M.fill(10.0)
+M = QArray.from_input(A, unit=m ** 2)
 
 C = QArray(shape=(nx, ny), unit=cm ** 2)
 C.fill(1.0e4)
 
-S = QArray(shape=(nx, ny), unit=s)
-S.fill(10.0)
+S = QArray.from_input(A, unit=s)
 
 R = QArray(shape=(nx, ny), unit=m ** 2)
 R.fill(11)
@@ -32,7 +30,6 @@ A1 = UnitArraySci(A)
 M1 = UnitArraySci(A, units=meter * meter)
 C1 = UnitArraySci(C, units=centimeter * centimeter)
 S1 = UnitArraySci(A, units=second)
-
 R1 = UnitArraySci(R, units=meter * meter)
 
 
@@ -41,7 +38,7 @@ class TestAddition:
         def add():
             return M + C
 
-        benchmark.pedantic(add, iterations=1000, rounds=1)
+        benchmark.pedantic(add, iterations=700, rounds=4)
         assert add().unit == m ** 2
         assert array_equal(add(), R)
 
@@ -49,7 +46,7 @@ class TestAddition:
         def add():
             return M1 + C1
 
-        benchmark.pedantic(add, iterations=1000, rounds=1)
+        benchmark.pedantic(add, iterations=700, rounds=4)
         assert add().units == meter ** 2
         assert array_equal(add(), R1)
 
@@ -59,12 +56,12 @@ class TestMul:
         def expr():
             return M / S ** 2
 
-        benchmark.pedantic(expr, iterations=1000, rounds=1)
+        benchmark.pedantic(expr, iterations=700, rounds=4)
         assert expr().unit == m ** 2 * s ** -2
 
     def test_mult_unit_array(self, benchmark):
         def expr():
             return M1 / (S1 ** 2)
 
-        benchmark.pedantic(expr, iterations=1000, rounds=1)
+        benchmark.pedantic(expr, iterations=700, rounds=4)
         assert expr().units == meter ** 2 / second ** 2
