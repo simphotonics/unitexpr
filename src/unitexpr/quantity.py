4db93dff60ef416b4aeefdf21a8a9236cf5d499e
simphotonics <git@simphotonics.com>
2021-12-14 00:13:16 +0000

    Quantity is now a scalar.

--- src/unitexpr/quantity.py
+++ src/unitexpr/quantity.py
@@ -1,83 +1,287 @@
+from __future__ import annotations
+
 from numbers import Number
+from typing import Callable, Union
 
-from .qarray import QArray
+from .errors import OperationNotSupported
+from .unit import UnitBase, UnitExprBase
 
 
-class Quantity(QArray):
+class Quantity:
     """
-    A quantity described by
-    a numerical value, a unit and an optional string
-    containing additional information.
-
-    Equivalent to a `QArray` with shape (1,).
-
-    The instance attribute `info` can be used to store
-    object documentation.
+    A quantity that can be described by a numerical value
+    of type `int`, `float`, or `complex` and a unit.
+    The attribute `info` can be used to store object information in the
+    form of a string.
     """
 
-    __slots__ = ("__info",)
+    _unit_types = (UnitBase, UnitExprBase)
 
-    def __new__(subtype, value: Number, unit=1.0, info=""):
-        obj = super().__new__(
-            subtype,
-            (1,),
-            dtype=type(value),
-            buffer=None,
-            offset=0,
-            strides=None,
-            unit=unit,
-        )
-        obj.fill(value)
-        obj.__info = info
+    __slots__ = ("__value", "__unit", "info")
+
+    def __new__(
+        cls,
+        value: Number,
+        unit: Union[UnitBase, UnitExprBase, float] = 1.0,
+        info: str = "",
+    ) -> Quantity:
+        return super().__new__(cls)
+
+    def __init__(
+        self,
+        value: Number,
+        unit: Union[UnitBase, UnitExprBase, float] = 1.0,
+        info: str = "",
+    ):
+        self.info = info
+        self.__value, self.__unit = self.__normalize(value, unit)
+        self.__unit = unit
+
+    @classmethod
+    def __idem(
+        cls,
+        value: Number,
+        unit: Union[UnitBase, UnitExprBase, float] = 1.0,
+        info: str = "",
+    ) -> Quantity:
+        """
+        Constructs an object of type `Quantity` without normalizing the unit.
+        """
+        obj = super().__new__(cls)
+        obj.__value = value
+        obj.__unit = unit
+        obj.info = info
         return obj
 
-    def __array_finalize__(self, obj):
-        if obj is None:
-            return
-        self.unit = getattr(obj, "unit", 1.0)
+    @property
+    def unit(self):
+        """Returns the unit of the quantity."""
+        return self.__unit
 
     @property
     def value(self):
-        """ Copies the entry stored at `self[0]` to a
-        standard Python scalar and returns the result.
-        """
-        return self.item()
-
-    @property
-    def info(self):
-        try:
-            return self.__info
-        except AttributeError:
-            return ""
-
-    def __str__(self) -> str:
-        return f"{self[0]} {self.unit}"
-
-    def __repr__(self) -> str:
-        return (
-            f"{self.__class__.__name__}({self[0]}, "
-            + f"unit={self.unit}, info={self.info.__repr__()})"
-        )
-
-    def min(self):
-        return self[0]
-
-    def max(self):
-        return self[0]
-
-    def mean(self):
-        return self[0]
+        """Returns the numerical value of the quantity."""
+        return self.__value
 
     @classmethod
-    def from_input(cls, input, unit=1.0):
-        raise NotImplementedError()
+    def __normalize(cls, value, unit) -> tuple:
 
-    def reshape(self, shape, order):
-        raise NotImplementedError(
-            f"Objects of type '{self.__class__.__name__}' cannot be reshaped."
+        if not isinstance(value, Number):
+            raise TypeError(
+                f"Parameter 'value' must be numerical. Found {value}."
+            )
+
+        try:
+            factor = (
+                unit.factor
+                if isinstance(unit, cls._unit_types)
+                else float(unit)
+            )
+        except ValueError as error:
+            raise ValueError(
+                "Expected a 'unit', a 'unit expression', or input that be "
+                + f"converted to a float. Found:{unit.__repr__()}."
+            ) from error
+
+        if factor == 1.0:
+            return (value, unit)
+
+        if factor == 0.0:
+            raise ValueError(
+                f"Could not set unit with zero magnitude: {value}."
+            )
+        return (value * factor, unit / factor)
+
+    def __str__(self) -> str:
+        return f"{self.__value} {self.__unit}"
+
+    def __repr__(self) -> str:
+        result = (
+            f"{self.__class__.__name__}({self.__value}, "
+            + f"unit={self.__unit}"
         )
 
-    def resize(self, new_shape, refcheck=True):
-        raise NotImplementedError(
-            f"Objects of type '{self.__class__.__name__}' cannot be resized."
+        if self.info.strip():
+            result += f", info={self.info.__repr__()})"
+        else:
+            result += ")"
+        return result
+
+    def copy(self):
+        return self.__idem(self.__value, self.__unit, self.info)
+
+    def __add__(self, other) -> Quantity:
+        other_unit = getattr(other, "unit", 1.0)
+        other_value = getattr(other, "value", other)
+
+        # If units match simply add values.
+        if self.__unit == other_unit:
+            return self.__idem(self.__value + other_value, self.__unit)
+
+        alpha = other_unit / self.__unit
+
+        if isinstance(alpha, float):
+            return self.__idem(self.__value + other_value * alpha, self.__unit)
+
+        if (
+            isinstance(alpha, UnitExprBase)
+            and alpha.base_exponents == alpha.base_exponents_zero
+        ):
+            return self.__idem(
+                self.__value + other_value * alpha.base_factor, self.__unit
+            )
+        raise OperationNotSupported(self, other, "+")
+
+    def __sub__(self, other) -> Quantity:
+        other_unit = getattr(other, "unit", 1.0)
+        other_value = getattr(other, "value", other)
+
+        # If units match simply subtract values.
+        if self.__unit == other_unit:
+            return self.__idem(self.__value - other_value, self.__unit)
+
+        alpha = other_unit / self.__unit
+
+        if isinstance(alpha, float):
+            return self.__idem(self.__value - other_value * alpha, self.__unit)
+
+        if (
+            isinstance(alpha, UnitExprBase)
+            and alpha.base_exponents == alpha.base_exponents_zero
+        ):
+            return self.__idem(
+                self.__value - other_value * alpha.base_factor, self.__unit
+            )
+        raise OperationNotSupported(self, other, "-")
+
+    def __mul__(self, other) -> Quantity:
+        """
+        Returns the result of multiplying `self` with `other`.
+        """
+        if isinstance(other, self._unit_types):
+            return self.__class__(self.__value, self.__unit * other)
+
+        other_unit = getattr(other, "unit", 1.0)
+        other_value = getattr(other, "value", other)
+        try:
+            return self.__class__(
+                self.__value * other_value, self.__unit * other_unit
+            )
+        except TypeError:
+            return NotImplemented
+
+    def __rmul__(self, other) -> Quantity:
+        """
+        Returns the result of multiplying `other` with `self`.
+        """
+        if isinstance(other, self._unit_types):
+            return self.__class__(self.__value, other * self.__unit)
+
+        other_unit = getattr(other, "unit", 1.0)
+        other_value = getattr(other, "value", other)
+        return self.__class__(
+            other_value * self.__value,
+            other_unit * self.__unit,
         )
+
+    def __truediv__(self, other) -> Quantity:
+        """
+        Returns the result of dividing the `self` by `other`.
+        """
+        if isinstance(other, self._unit_types):
+            return self.__class__(self.__value, self.__unit / other)
+
+        other_unit = getattr(other, "unit", 1.0)
+        other_value = getattr(other, "value", other)
+
+        try:
+            return self.__class__(
+                self.__value / other_value,
+                self.__unit / other_unit,
+            )
+        except TypeError:
+            return NotImplemented
+
+    def __rtruediv__(self, other) -> Quantity:
+        """
+        Returns the result of dividing the `other` by `self`.
+        """
+        if isinstance(other, self._unit_types):
+            return self.__class__(self.__value, other / self.__unit)
+
+        other_unit = getattr(other, "unit", 1.0)
+        other_value = getattr(other, "value", other)
+        return self.__class__(
+            other_value / self.__value,
+            other_unit / self.__unit,
+        )
+
+    def __pow__(self, other: Union[float, int]) -> Quantity:
+        return self.__class__(self.__value ** other, self.__unit ** other)
+
+    def __abs__(self) -> Quantity:
+        return self.__idem(self.__value.__abs__(), self.__unit)
+
+    def __neg__(self) -> Quantity:
+        return self.__idem(-self.__value, self.__unit)
+
+    def __pos__(self) -> Quantity:
+        return self.__idem(self.__value.__pos__(), self.__unit)
+
+    def __eq__(self, other) -> bool:
+        result = self.compare(other, self.__value.__eq__)
+        if result is None:
+            return False
+        return result
+
+    def __le__(self, other) -> bool:
+        result = self.compare(other, self.__value.__le__)
+        if result is None:
+            raise OperationNotSupported(self, other, "<=")
+        return result
+
+    def __ge__(self, other) -> bool:
+        result = self.compare(other, self.__value.__ge__)
+        if result is None:
+            raise OperationNotSupported(self, other, ">=")
+        return result
+
+    def __gt__(self, other) -> bool:
+        result = self.compare(other, self.__value.__gt__)
+        if result is None:
+            raise OperationNotSupported(self, other, ">")
+        return result
+
+    def __lt__(self, other) -> bool:
+        result = self.compare(other, self.__value.__lt__)
+        if result is None:
+            raise OperationNotSupported(self, other, "<")
+        return result
+
+    def compare(
+        self, other, comparison_operator: Callable
+    ) -> Union[bool, None]:
+        """
+        Generic comparison function that handles input of type `Number`,
+        `ndarray` and `QArray` using `comparison_operator`.
+
+        Returns `None` if the comparison failed due to incompatible units.
+        """
+        other_unit = getattr(other, "unit", 1.0)
+        other_value = getattr(other, "value", other)
+
+        if self.__unit == other_unit:
+            return comparison_operator(other_value)
+
+        alpha = other_unit / self.__unit
+
+        if isinstance(alpha, float):
+            return comparison_operator(other_value * alpha)
+
+        if (
+            isinstance(alpha, UnitExprBase)
+            and alpha.base_exponents == alpha.base_exponents_zero
+        ):
+            return comparison_operator(other_value * alpha.base_factor)
+
+        return None
