4a99c9acdfc75e70e63fb4c158ada8e0f3ecf8a0
simphotonics <git@simphotonics.com>
2021-12-17 10:24:22 +0000

    Corrected link to repository.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Units, unit expressions, and united arrays.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/simphotonics/units",
+    url="https://github.com/simphotonics/unitexpr",
     project_urls={
         "Bug Tracker": "https://github.com/simphotonics/unitexpr/issues",
     },
